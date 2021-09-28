c255cf06c687cda67903c0be0cdea2f21e6989ab
Christine Lo <christine.lo.hayes@gmail.com>
2021-09-27 21:57:20 -0400

    part 2

--- express_server.js
+++ express_server.js
@@ -16,28 +16,35 @@ function generateRandomString(len) {
 }
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString(6);
+  let longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
-
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-const urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL:req.params.longURL };
+  console.log(req.params.shortURL);
+  const templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/", (req, res) => {
